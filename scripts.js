6ff13dc9389a03be74fb943cbc5a1517f4a610c7
srinupotnuru <iamsrinu3@gmail.com>
2020-05-26 14:26:35 +0530

    refresh

---
+++ scripts.js
@@ -0,0 +1,44 @@
+const app = document.getElementById("root");
+
+const logo = document.createElement("img");
+logo.src = "logo.gif";
+
+const container = document.createElement("div");
+container.setAttribute("class", "container");
+
+app.appendChild(logo);
+app.appendChild(container);
+
+var request = new XMLHttpRequest();
+request.open("GET", "https://api.covidindiatracker.com/state_data.json", true);
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach((stateOB) => {
+      const card = document.createElement("div");
+      card.setAttribute("class", "card");
+
+      const h1 = document.createElement("h1");
+      h1.textContent = stateOB.state;
+
+      const p = document.createElement("p");
+      p.textContent = `Confirmed :${stateOB.confirmed}\n Recovered : ${stateOB.recovered}
+      Deaths :${stateOB.deaths}`;
+
+      container.appendChild(card);
+      card.onclick = () => {
+        let path = `state.html?${stateOB.state}`;
+        window.open(path, "_self");
+      };
+      card.appendChild(h1);
+      card.appendChild(p);
+    });
+  } else {
+    const errorMessage = document.createElement("marquee");
+    errorMessage.textContent = `Gah, it's not working!`;
+    app.appendChild(errorMessage);
+  }
+};
+
+request.send();
---
+++ stateOB.js
@@ -0,0 +1,47 @@
+const app = document.getElementById("root");
+
+const logo = document.createElement("img");
+logo.src = "logo.gif";
+
+const container = document.createElement("div");
+container.setAttribute("class", "container");
+
+app.appendChild(logo);
+app.appendChild(container);
+
+var request = new XMLHttpRequest();
+request.open("GET", "https://api.covidindiatracker.com/state_data.json", true);
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  if (request.status >= 200 && request.status < 400) {
+    let state = window.location.href.split("?")[1];
+
+    if (state.includes("%20")) {
+      state = state.replace("%20", " ");
+    }
+    data
+      .filter((s) => s.state == state)[0]
+      .districtData.forEach((stateOB) => {
+        const card = document.createElement("div");
+        card.setAttribute("class", "card");
+
+        const h1 = document.createElement("h1");
+        h1.textContent = stateOB.name;
+
+        const p = document.createElement("p");
+        p.textContent = `Confirmed :${stateOB.confirmed}\n Recovered : ${stateOB.recovered}
+      Deaths :${stateOB.deaths}`;
+
+        container.appendChild(card);
+        card.appendChild(h1);
+        card.appendChild(p);
+      });
+  } else {
+    const errorMessage = document.createElement("marquee");
+    errorMessage.textContent = `Gah, it's not working!`;
+    app.appendChild(errorMessage);
+  }
+};
+
+request.send();
